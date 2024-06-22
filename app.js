8e36aa7482b0156cad994863c0bd84a647e5f330
Mert Ali CİRHİNLİOĞLU <mert_ali_58cir@hotmail.com>
2024-06-22 13:34:19 +0300

    lesson002

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//ejs template engine
+app.set('view engine', 'ejs');
+
+
+// static files middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('İndex sayfası!');
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
+
+
 
